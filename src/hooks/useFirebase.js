33c3cb83c4b6be4f1d6344d92635b8ddceb33051
Nahidjc <nahidjc80@gmail.com>
2021-10-18 19:20:03 +0600

    fake json data

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,39 +1,39 @@
 import {
-    getAuth,
-    signInWithPopup,
-    signOut,
-    signInWithEmailAndPassword,
-    GoogleAuthProvider,
-} from 'firebase/auth';
-import { useState } from 'react';
+  getAuth,
+  signInWithPopup,
+  signOut,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { useState } from "react";
+import initializeAuthentication from "./../Firebase/firebase.initialize";
 
+initializeAuthentication();
 
 const useFirebase = () => {
-    const [user, setUser] = useState({})
-    const auth = getAuth();
-    const googleProvider = new GoogleAuthProvider();
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
 
-    // Popup login system
-    const signInUsingGoogle = () => {
-        signInWithPopup(auth, googleProvider)
-            .then(result => {
-                const user = result.user;
-                setUser(user)
-            })
-            .catch(error => {
-                const errorMessage = error.message;
-                console.log(errorMessage);
-            });
-    };
+  // Popup login system
+  const signInUsingGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
 
-    const logOut = () => {
-        signOut(auth)
-            .then(() => {
-                setUser({})
-            })
-
-    }
-    return { user, signInUsingGoogle, logOut }
-}
+  const logOut = () => {
+    signOut(auth).then(() => {
+      setUser({});
+    });
+  };
+  return { user, signInUsingGoogle, logOut };
+};
 
 export default useFirebase;
