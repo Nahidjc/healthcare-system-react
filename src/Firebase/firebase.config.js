b031f205292e7858ebff9b7e23b2db08f959bb30
Nahidjc <nahidjc80@gmail.com>
2021-10-18 14:44:44 +0600

    firebase authentication initiliaze

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAiNs-PYGDFwMIsOw-NYALO7HaYs0uA2Cs",
+    authDomain: "healthcare-system-nahid.firebaseapp.com",
+    projectId: "healthcare-system-nahid",
+    storageBucket: "healthcare-system-nahid.appspot.com",
+    messagingSenderId: "389243557279",
+    appId: "1:389243557279:web:c9e06feefffe1b39ae19aa"
+  };
+
+  export default firebaseConfig;
